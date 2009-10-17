f81719609daf87a99a228795241d76ce151d981c
Alexandr Ciornii <alexchorny@gmail.com>
2009-10-17 13:46:02 +0300

    boilerplate code

---
+++ NGP.pm
@@ -0,0 +1,9 @@
+package NGP;
+use 5.006;
+use strict;
+use warnings;
+
+
+
+1;
+# (c) Alexandr Ciornii, 2009
---
+++ test.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+use 5.006;
+use strict;
+use warnings;
+
+
+
+# (c) Alexandr Ciornii, 2009
