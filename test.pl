65985d6f1a196a33400332e0c4426076137a09ab
Alexandr Ciornii <alexchorny@gmail.com>
2009-11-08 12:13:56 +0200

    use Test::More in main test

--- test.pl
+++ test.pl
@@ -4,12 +4,17 @@ use 5.006;
 use strict;
 use warnings;
 use lib 'lib';
+use Test::More tests => 2;
 use Github::Fork::Parent;
 
-print github_parent('git://github.com/chorny/cgi-session.git'),"\n";
+is(github_parent('git://github.com/chorny/cgi-session.git'),
+ 'http://github.com/cromedome/cgi-session');
+#print github_parent('git://github.com/chorny/cgi-session.git'),"\n";
 #git://github.com/cromedome/cgi-session.git
 
-print github_parent('git@github.com:chorny/PPI-App-ppi_version-BDFOY.git'),"\n";
+is(github_parent('git@github.com:chorny/PPI-App-ppi_version-BDFOY.git'),
+ 'http://github.com/briandfoy/PPI-App-ppi_version-BDFOY');
+#print github_parent('git@github.com:chorny/PPI-App-ppi_version-BDFOY.git'),"\n";
 #git://github.com/briandfoy/PPI-App-ppi_version-BDFOY.git
 
 
